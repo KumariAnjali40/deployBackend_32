2a40039da7e5e6fd484ade9e3e7693b13bd496fe
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-01-10 15:30:05 +0530

    cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express=require('express');
 const { connection } = require("./db");
 const {userRouter}=require('./routes/user.routes');
 // const jwt=require('jsonwebtoken');
-const {noteRouter}=require('./routes/notes.routes')
+const {noteRouter}=require('./routes/notes.routes');
+const cors=require('cors');
 const app=express();
 app.use(express.json());
-
+app.use(cors());
 
 app.use('/users',userRouter);
 app.use('/notes',noteRouter)
